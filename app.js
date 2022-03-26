61f104f0cd713fd0ecc6728d66d82eb9f3184ec4
Mia Jarnot <ejarnot42@outlook.com>
2022-03-26 12:11:31 -0700

    Finished exercises 1 and 2. Was not present in class for 3, 4, and 5

--- app.js
+++ app.js
@@ -2,6 +2,27 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+function printOdds(count){
+    for (let number = 1; number <= 100; number ++ ){
+        if(number % 2 !== 0){
+            console.log(`${number} and counting...`)
+        }
+    }
+}
+printOdds(100)
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+function checkAge(age, name = "Stranger"){
+    let aboveSixteen = `Congrats, ${name}! You can drive!`
+    let belowSixteen = `Sorry, ${name}, but you need to wait until you're 16.`
+    if(age < 16){
+        console.log(belowSixteen);
+    }
+    if (age >= 16){
+        console.log(aboveSixteen);
+    }
+}
+checkAge(17, "Mia")
+checkAge(15)
